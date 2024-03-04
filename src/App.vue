d1b490a49a0f8f9ea469e2441a3e256d37a3e354
christiansaccani <christian.saccani99@gmail.com>
2024-03-04 17:31:33 +0100

    store done

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,27 @@
 // per importare axios 
 import axios from 'axios';
 
+import {store} from './store.js';
+
 export default {
   data() {
     return {
+      cards: [],
 
+      store,
     }
   },
 
   created() {
     // spazio di codice che viene eseguito appena l'applicazione viene lanciata
     
+    // da qui riempio il mio array con i dati delle carte
     axios
-      .get('https://rickandmortyapi.com/api/character?page=1')
+      .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
       .then(res => {
-        console.log(res.data.results)
-        this.store.characters = res.data.results
-    }).catch(err => {
-      console.log(err)
+        console.log(res.data)
+        this.store.cards = res.data
+        console.log(this.store.cards)
     })
 
   },
