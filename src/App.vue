9b102996e11fb950a5ff2ce8566a6c88df2bcb1e
christiansaccani <christian.saccani99@gmail.com>
2024-03-04 17:05:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+// per importare axios 
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  created() {
+    // spazio di codice che viene eseguito appena l'applicazione viene lanciata
+    
+    axios
+      .get('https://rickandmortyapi.com/api/character?page=1')
+      .then(res => {
+        console.log(res.data.results)
+        this.store.characters = res.data.results
+    }).catch(err => {
+      console.log(err)
+    })
+
+  },
+
+  components: {
+    
+  },
+
+  methods: {
+
+
+  },
+}
+</script>
+<!-- ---------------------------------------------------------------------------------- -->
+<template>
+
+
+
+</template>
+<!-- ---------------------------------------------------------------------------------- -->
+<style lang="scss">
+
+
+
+</style>
